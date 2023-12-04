b607802aa004964091e206ae8ca5d4f37b1e353b
Lukis <129191678+LucasGerez03@users.noreply.github.com>
2023-12-04 20:03:57 -0300

    Add files via upload

---
+++ flowers/main.js
@@ -0,0 +1,17 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+prompt("ODA matuli ¿como estas?")
+alert("meno yo muy bien por suerte")
+alert("te queria decir que sos alguien muy especial para mi :)")
+prompt("no se si sabias, pero yo te amo mucho ¿lo sabias? (Conteshte con si o no)")
+alert("desgraciadamente... no tengo la habilidad para tejerte flores que te duren para siempre :(")
+alert("asi que penseee")
+alert("y pensee...(piensa en chichilo)")
+alert("y segui pensando (un poquito mas)")
+alert("hasta que dijee, aaa tengo una idea")
+alert("no puedo tejerlo, pero si programarlo")
+alert("asi que te quiero dedicar")
+alert("estas flores, que son iguales a mi amor por vos")
+alert("porque nunca se va a marchitar :D")
